ff230a6ea7a95c383818ffc01cbba2b024077231
RangerBot-OP <98682482+THERANGER-BOT@users.noreply.github.com>
2022-02-06 19:41:06 +0530

    Update Ranger.py

--- Ranger.py
+++ Ranger.py
@@ -6,8 +6,8 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://gitHub.com/TeamLionX/LionX && cd LionX && python3 -m userbot")
+system("git clone https://github.com/THERANGER-BOT/RangerBotOP && cd  Ranger && python3 -m userbot")
 # if got error
-print(" Sorry ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sorry ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @The_RangerBot")
 # Script written for stop heroku suspension
 # use with credit else gay
